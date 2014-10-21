8db2dae59676ac05a986748db06633e181553b71
aha <ntuaha@gmail.com>
2014-10-22 01:39:12 +0800

    新增對應表

--- sql/vote2election.sql
+++ sql/vote2election.sql
@@ -1,5 +1,10 @@
 drop table vote2election;
 create table vote2election(
-  vote_id integer,
-  election_id varchar
+vote_id integer,
+election_id varchar,
+area  varchar,
+name  varchar,
+main_type varchar,
+sub_type  varchar,
+term  integer
 );
--- src/vote2election.py
+++ src/vote2election.py
@@ -80,197 +80,192 @@ class VOTE2ELECTION:
 		self.conn.close()
 
 	#建立更新資料庫
-	def rebuildTable(self,sql):
+	def rebuildTable(self):
 		print '執行重建Table'
 		sql = os.path.abspath('../sql/vote2election.sql')
 		os.system('psql -d %s -f %s'%(self.database,sql))
 
 	def map(self):
-		sql = "SELECT A.election_id,A.year,A.main_type,C.name from election A left join election_attr C on A.election_id = C.election_id"
+		sql = "SELECT A.election_id,A.area,A.name,B.main_type,B.sub_type,B.term FROM election_record AS A LEFT JOIN election AS B ON A.election_id = B.election_id "
 		self.cur.execute(sql)
-		for row in self.cur.fetchall():
-			sql = "SELECT vote_id,title from votecash where title like '%%%s%%' and title like '%%%s%%'  and cond_name = "
+		results = self.cur.fetchall()
+		for row in results:
+			(election_id ,area,name,main_type,sub_type,term)= row
+
+			if main_type =='總統' and sub_type=='選舉':
+				term = '%02d'%int(term)
+				print "PRESIDENT"
+				# 屆數
+				sql = "SELECT vote_id,title from votecash where title like '%%%s%%'  and split_part(cand_name,'、',1) = '%s'"%(term,name)
+				#print sql
+				self.cur.execute(sql)
+				r2 = self.cur.fetchall()
+				for rr in r2:
+					sql = "INSERT INTO vote2election (vote_id, election_id, area, name, main_type, sub_type, term) VALUES (%s,'%s','%s','%s','%s','%s',%s)"%(rr[0],election_id,area,name,main_type,sub_type,term)
+					#print "rr: "+sql
+					self.cur.execute(sql)
+					self.conn.commit()
+
+			elif main_type =='立法委員' and sub_type=='選舉':
+				term = '%d'%int(term)
+				# 屆數
+				sql = "SELECT vote_id,title from votecash where title like '%%%s%%'  and title like '%%%s%%'  and split_part(cand_name,'、',1) = '%s'"%(term,main_type,name)
+				#print sql
+				self.cur.execute(sql)
+				r2 = self.cur.fetchall()
+				for rr in r2:
+
+					sql = "INSERT INTO vote2election (vote_id, election_id, area, name, main_type, sub_type, term) VALUES (%s,'%s','%s','%s','%s','%s',%s)"%(rr[0],election_id,area,name,main_type,sub_type,term)
+#					print "rr: "+sql
+					self.cur.execute(sql)
+					self.conn.commit()
+			elif main_type =='直轄市長' and sub_type=='選舉':
+				if (area=='新北市' or area=='高雄市' or area=='臺中市' or area=='臺南市') and term>95:
+					term = "%d"%((int(term)-95)/4)
+				else:
+					term = '%d'%((int(term)-79)/4)
+
+				# 屆數
+				sql = "SELECT vote_id,title from votecash where title like '%%%s%%'  and title like '%%%s%%' and split_part(cand_name,'、',1) = '%s'"%(term,area,name)
+#				print sql
+				self.cur.execute(sql)
+				r2 = self.cur.fetchall()
+				for rr in r2:
+					sql = "INSERT INTO vote2election (vote_id, election_id, area, name, main_type, sub_type, term) VALUES (%s,'%s','%s','%s','%s','%s',%s)"%(rr[0],election_id,area,name,main_type,sub_type,term)
+#					print "rr: "+sql
+					self.cur.execute(sql)
+					self.conn.commit()
+
+			elif main_type =='縣市長' and	 sub_type=='選舉':
+				#print term
+				#print area
+				if area=='連江縣':
+					term = '%d'%((int(term)-78)/4)
+				else:
+					term = '%d'%((int(term)-34)/4)
+				# 屆數
+				sql = "SELECT vote_id,title from votecash where title like '%%%s%%'  and title like '%%%s%%' and split_part(cand_name,'、',1) = '%s'"%(term,area,name)
+#				print sql
+				self.cur.execute(sql)
+				r2 = self.cur.fetchall()
+				for rr in r2:
+#					print rr
+					sql = "INSERT INTO vote2election (vote_id, election_id, area, name, main_type, sub_type, term) VALUES (%s,'%s','%s','%s','%s','%s',%s)"%(rr[0],election_id,area,name,main_type,sub_type,term)
+					#print "rr: "+sql
+					self.cur.execute(sql)
+					self.conn.commit()
+
+			elif main_type =='鄉鎮市長' and sub_type=='選舉':
+				term2 = term
+				term = '%d'%((int(term)-78)/4)
+				title = area.decode('utf-8')[-1]+"長"
+				# 屆數
+				sql = "SELECT vote_id,title from votecash where title like '%%%s%%'  and title like '%%%s%%'  and  title like '%%%s%%' and split_part(cand_name,'、',1) = '%s'"%(term,area,title,name)
+#				print sql
+				self.cur.execute(sql)
+				r2 = self.cur.fetchall()
+				for rr in r2:
+#					print rr
+					sql = "INSERT INTO vote2election (vote_id, election_id, area, name, main_type, sub_type, term) VALUES (%s,'%s','%s','%s','%s','%s',%s)"%(rr[0],election_id,area,name,main_type,sub_type,term)
+#					print "rr: "+sql+"\n"
+					self.cur.execute(sql)
+					self.conn.commit()
+
+				if len(r2) == 0:
+					term = '%d'%((int(term2)-34)/4)
+					sql = "SELECT vote_id,title from votecash where title like '%%%s%%'  and title like '%%%s%%'  and  title like '%%%s%%' and split_part(cand_name,'、',1) = '%s'"%(term,area,title,name)
+#					print sql
+					self.cur.execute(sql)
+					r2 = self.cur.fetchall()
+					for rr in r2:
+#						print rr
+						sql = "INSERT INTO vote2election (vote_id, election_id, area, name, main_type, sub_type, term) VALUES (%s,'%s','%s','%s','%s','%s',%s)"%(rr[0],election_id,area,name,main_type,sub_type,term)
+#						print "rr: "+sql+"\n"
+						self.cur.execute(sql)
+						self.conn.commit()
 
 
-	def query_cand_from_github(self,parm):
-		[self.election_id,position,title] = parm
-		if title.find("不分區")>0 or title.find("任務型國大代表")>0:
-			return
-		url = "https://raw.githubusercontent.com/kiang/db.cec.gov.tw/master/elections/%s.csv"%self.election_id
-		print url
-		response = urllib2.build_opener().open(url)
-		the_page = response.read()
-		response.close()
-		lines = the_page.split("\n")
-		print parm
-		for line in lines[1:] :
-			cols = line.split(",")
+			elif main_type =='村里長' and sub_type=='選舉':
+				print term
+				print area
+				term2 = term
+				term = '%d'%((int(term)-78)/4)
+				title = area.decode('utf-8')[-1]+"長"
+				# 屆數
+				sql = "SELECT vote_id,title from votecash where title like '%%%s%%'  and title like '%%%s%%'  and  title like '%%%s%%' and split_part(cand_name,'、',1) = '%s'"%(term,area,title,name)
+#				print sql
+				self.cur.execute(sql)
+				r2 = self.cur.fetchall()
+				for rr in r2:
+#					print rr
+					sql = "INSERT INTO vote2election (vote_id, election_id, area, name, main_type, sub_type, term) VALUES (%s,'%s','%s','%s','%s','%s',%s)"%(rr[0],election_id,area,name,main_type,sub_type,term)
+#					print "rr: "+sql+"\n"
+					self.cur.execute(sql)
+					self.conn.commit()
 
-			if len(cols)<len(lines[0].split(",")):
-				break
+				if len(r2) == 0:
+					term = '%d'%((int(term2)-34)/4)
+					sql = "SELECT vote_id,title from votecash where title like '%%%s%%'  and title like '%%%s%%'  and  title like '%%%s%%' and split_part(cand_name,'、',1) = '%s'"%(term,area,title,name)
+#					print sql
+					self.cur.execute(sql)
+					r2 = self.cur.fetchall()
+					for rr in r2:
+#						print rr
+						sql = "INSERT INTO vote2election (vote_id, election_id, area, name, main_type, sub_type, term) VALUES (%s,'%s','%s','%s','%s','%s',%s)"%(rr[0],election_id,area,name,main_type,sub_type,term)
+#						print "rr: "+sql+"\n"
+						self.cur.execute(sql)
+						self.conn.commit()
 
-			[area,name,no,gender,birth_year,party,vote_cnt,vote_rate,elect_ind,incumbent_Ind]= cols
+			elif main_type =='直轄市里長' and sub_type=='選舉':
+				print term
+				print area
+				term2 = term
+				term = '%d'%((int(term)-75)/4)
+				title = area.decode('utf-8')[-1]+"長"
+				# 屆數
+				sql = "SELECT vote_id,title from votecash where title like '%%%s%%'  and title like '%%%s%%'  and  title like '%%%s%%' and split_part(cand_name,'、',1) = '%s'"%(term,area,title,name)
+				print sql
+				self.cur.execute(sql)
+				r2 = self.cur.fetchall()
+				for rr in r2:
+					print rr
+					sql = "INSERT INTO vote2election (vote_id, election_id, area, name, main_type, sub_type, term) VALUES (%s,'%s','%s','%s','%s','%s',%s)"%(rr[0],election_id,area,name,main_type,sub_type,term)
+					print "rr: "+sql+"\n"
+					self.cur.execute(sql)
+					self.conn.commit()
 
-			[sec_name,sec_gender,sec_birth_year] = ["","","NULL"]
-			if elect_ind == '':
-				elect_ind = "false"
-			else:
-				elect_ind = "true"
-
-			if incumbent_Ind =="是":
-				incumbent_Ind = "true"
-			else:
-				incumbent_Ind = "false"
-
-			if position =="總統":
-				[name,sec_name] = cols [1].split("|")
-				[gender,sec_gender] = cols [3].split("|")
-				[birth_year,sec_birth_year] = cols [4].split("|")
-
-			if gender =="男":
-				gender  = "M"
-			else:
-				gender = "F"
-
-			if sec_gender =="男":
-				sec_gender  = "M"
-			elif sec_gender =="女":
-				sec_gender = "F"
-			name=name.replace("\"","")
-			vote_rate = vote_rate[:-1]
-			c =",".join( ["election_id", "area", "no", "name", "gender", "birth_year", "sec_name", "sec_gender", "sec_birth_year", "party", "vote_cnt", "vote_rate", "elect_ind", "incumbent_Ind"])
-			sql = "INSERT INTO election_record (%s ) VALUES ('%s','%s',%s,'%s','%s',%s,'%s','%s',%s,'%s',%s,%s/100,%s,%s)"%(c,self.election_id, area, no, name, gender, birth_year, sec_name, sec_gender, sec_birth_year, party, vote_cnt, vote_rate, elect_ind, incumbent_Ind)
-			print sql
-			self.cur.execute(sql)
-			self.conn.commit()
-
-
-
-
-
-
-	def query_cand(self,input):
-		self.election_id = input[0]
-		typ = "ctks"
-		#grab web
-		url = 'http://db.cec.gov.tw/histQuery.jsp?voteCode=%s&qryType=%s'%(self.election_id,typ)
-		print url
-		response = urllib2.build_opener().open(url)
-		the_page = response.read()
-		response.close()
-		print the_page
-		page = self.__parse_html(the_page,typ)
-		position = input[1]
-		if position == "總統":
-			self.record_president(page)
-
-	def record_president(self,page):
-		pass
-
-	def query(self,election_id,position,title):
-		self.election_id = election_id
-		typ = "prof"
-		#grab web
-		url = 'http://db.cec.gov.tw/histQuery.jsp?voteCode=%s&qryType=%s'%(election_id,typ)
-		print url
-		response = urllib2.build_opener().open(url)
-		the_page = response.read()
-		response.close()
-		page = self.__parse_html(the_page,typ)
-		if position=="總統":
-			print "GO PRESIDENT"
-			self.attr_president(page)
-		elif title.find("不分區")>0 or title.find('任務型國大')>0:
-			self.attr_other_special(page)
-		else:
-			self.attr_other(page)
-
-	def __parse_html(self,the_page,typ):
-		lines = the_page.split("\n")
-		flag = False
-		result = ["<html><body>"]
-		for line in lines:
-			if line.find('<table class="%s"'%typ)>0:
-				flag = True
-
-			if flag==True and line.find('<!--')<0:
-				result.append(line)
-
-			if line.find('</table>')>0 :
-				flag = False
-		result.append("</body></html>")
-		result = "\n".join(result)
-		print result
-		return html.fromstring(StringIO.StringIO(result).getvalue().decode("utf-8"))
-
-	def attr_other_special(self,page):
-		tds = page.xpath('//table/tr[3]/td')
-
-		cols =",".join( ["election_id", "area", "population", "election_cnt", "candidate_cnt",  "vote_cnt", "vote_valid", "vote_invalid", "elect_pop_rate", "vote_elect_rate"] )
-		area = tds[0].xpath("./a")[0].text.encode("utf-8")
-		population = tds[1].text
-		election_cnt = tds[2].text
-		candidate_cnt= tds[3].text
-
-		vote_cnt= tds[4].text
-		vote_valid= tds[5].text
-		vote_invalid= tds[6].text
-		elect_pop_rate= tds[7].text[:-1]
-		vote_elect_rate= tds[8].text[:-1]
-		print (cols,self.election_id, area, population, election_cnt, candidate_cnt, vote_cnt, vote_valid, vote_invalid, elect_pop_rate, vote_elect_rate)
-		sql = "INSERT INTO election_attr(%s) VALUES ('%s','%s',%s,%s,%s,%s,%s,%s,%s/100,%s/100)"%(cols,self.election_id, area, population, election_cnt, candidate_cnt, vote_cnt, vote_valid, vote_invalid, elect_pop_rate, vote_elect_rate)
-		print sql
-		self.cur.execute(sql)
-		self.conn.commit()
-
-	def attr_other(self,page):
-
-		tds = page.xpath('//table/tr[3]/td')
-
-		cols =",".join(["election_id", "area", "population", "election_cnt", "candidate_cnt", "candidate_male", "candidate_female", "winner_cnt", "winner_male", "winner_female", "vote_cnt", "vote_valid", "vote_invalid", "elect_pop_rate", "vote_elect_rate", "cand_win_rate"])
+				if len(r2) == 0:
+					term = '%d'%((int(term2)-95)/4)
+					sql = "SELECT vote_id,title from votecash where title like '%%%s%%'  and title like '%%%s%%'  and  title like '%%%s%%' and split_part(cand_name,'、',1) = '%s'"%(term,area,title,name)
+					print sql
+					self.cur.execute(sql)
+					r2 = self.cur.fetchall()
+					for rr in r2:
+						print rr
+						sql = "INSERT INTO vote2election (vote_id, election_id, area, name, main_type, sub_type, term) VALUES (%s,'%s','%s','%s','%s','%s',%s)"%(rr[0],election_id,area,name,main_type,sub_type,term)
+						print "rr: "+sql+"\n"
+						self.cur.execute(sql)
+						self.conn.commit()
 
 
-		area = tds[0].xpath("./a")[0].text.encode("utf-8")
-		population = tds[1].text
-		election_cnt = tds[2].text
-		candidate_cnt= tds[3].text
-		candidate_male = tds[4].text
-		candidate_female = tds[5].text
-		winner_male= tds[6].text
-		winner_female= tds[7].text
-		winner_cnt= tds[8].text
-		vote_cnt= tds[9].text
-		vote_valid= tds[10].text
-		vote_invalid= tds[11].text
-		elect_pop_rate= tds[12].text[:-1]
-		vote_elect_rate= tds[13].text[:-1]
-		cand_win_rate= tds[14].text[:-1]
-		sql = "INSERT INTO election_attr(%s) VALUES ('%s','%s',%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s/100,%s/100,%s/100)"%(cols,self.election_id, area, population, election_cnt, candidate_cnt, candidate_male, candidate_female, winner_cnt, winner_male, winner_female, vote_cnt, vote_valid, vote_invalid, elect_pop_rate, vote_elect_rate, cand_win_rate)
-		print sql
-		self.cur.execute(sql)
-		self.conn.commit()
 
 
-	def attr_president(self,page):
 
-		tds = page.xpath('//table/tr[3]/td')
 
-		cols =",".join( ["election_id", "area", "population", "election_cnt", "candidate_cnt", "winner_cnt",  "vote_cnt", "vote_valid", "vote_invalid", "elect_pop_rate", "vote_elect_rate", "cand_win_rate"] )
-		area = tds[0].xpath("./a")[0].text.encode("utf-8")
-		population = tds[1].text
-		election_cnt = tds[2].text
-		candidate_cnt= tds[3].text
-		winner_cnt= tds[4].text
-		vote_cnt= tds[5].text
-		vote_valid= tds[6].text
-		vote_invalid= tds[7].text
-		elect_pop_rate= tds[8].text[:-1]
-		vote_elect_rate= tds[9].text[:-1]
-		cand_win_rate= tds[10].text[:-1]
-		sql = "INSERT INTO election_attr(%s) VALUES ('%s','%s',%s,%s,%s,%s,%s,%s,%s,%s/100,%s/100,%s/100)"%(cols,self.election_id, area, population, election_cnt, candidate_cnt, winner_cnt, vote_cnt, vote_valid, vote_invalid, elect_pop_rate, vote_elect_rate, cand_win_rate)
-		print sql
-		self.cur.execute(sql)
-		self.conn.commit()
+
+ #國大代表
+ #直轄市里長
+ #鄉鎮市民代表
+ #直轄市議員
+# 臺灣省議員
+
+ #縣市議員
+
+# 臺灣省長
+
+
+
+
 
 
 
