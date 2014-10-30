719665124e4a370db673513a0a989e89d8d46531
aha <ntuaha@gmail.com>
2014-10-31 00:02:52 +0800

    修改首頁畫面

--- js/init.js
+++ js/init.js
@@ -10,6 +10,27 @@ function readData(){
 
 $(function(){
   readData();
+  $('body').scrollspy({ target: '.navbar-example',offset:80 });
+
+
+    $('a.navbar-brand').click(
+      function(){
+        $('html,body').animate({scrollTop:0}, 'slow');
+        return false;
+      }
+      );
+
+
+ var size = $('.pull-right  a').size();
+  for(var i =0;i<size;i++){
+    $($('.pull-right  a')[i]).click(
+      function(){
+        $('html,body').animate({scrollTop: $($(this).attr("href")).offset().top -70 }, 'slow');
+        return false;
+      }
+      );
+
+  }
 
 
 });
